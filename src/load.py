707334861dbeea993688269b19197d9e2b403321
Pablo Riquelme Santana de Souza <riquelmexxx506@gmail.com>
2025-04-04 15:55:30 -0300

    Atualização do README e docstring da função salvarCSV

--- src/load.py
+++ src/load.py
@@ -1,4 +1,18 @@
 import pandas as pd
 
-def saveCsv(df: pd.DataFrame, nome_arquivo: str, separador: str, decimal: str):
-    df.to_csv(f'src/datasets/{nome_arquivo}.csv', decimal=decimal, sep=separador)
+def salvarCSV(df, nome_arquivo):
+    """
+    Salva um DataFrame em um arquivo CSV.
+
+    Parâmetros:
+    df (DataFrame): O DataFrame a ser salvo.
+    nome_arquivo (str): Nome do arquivo de saída.
+
+    Retorno:
+    None
+    """
+    try:
+        df.to_csv(nome_arquivo, index=False, encoding="utf-8")
+        print(f"✅ Arquivo '{nome_arquivo}' salvo com sucesso!")
+    except Exception as e:
+        print(f"❌ Erro ao salvar CSV: {e}")
